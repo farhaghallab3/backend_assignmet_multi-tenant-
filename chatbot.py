9afaac835c74313451062432af4818fe5abc0e09
farhaghallab3 <farhagalab74@gmail.com>
2026-03-06 00:05:58 +0200

    update model gemeni

--- chatbot.py
+++ chatbot.py
@@ -6,7 +6,7 @@ from fastapi.responses import StreamingResponse
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 if GEMINI_API_KEY:
     genai.configure(api_key=GEMINI_API_KEY)
-    model = genai.GenerativeModel('gemini-1.5-flash')
+    model = genai.GenerativeModel('gemini-2.5-flash')
 else:
     model = None
 
